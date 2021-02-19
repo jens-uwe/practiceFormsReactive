1aeee9cac991eefa4b8c02526c07706e87ae7bd2
jens-uwe <jens-uwe@zenss.de>
2021-02-19 20:09:39 +0100

    start

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+
+  } 
+
+  onSubmit(): void {
+    
+  }
+
+}
